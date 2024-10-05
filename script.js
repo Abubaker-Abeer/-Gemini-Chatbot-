639930024d086f0ae18df9e044aa277b1f472263
abeer-abubaker <abeerabubaker2001@gmail.com>
2024-10-05 03:29:22 +0300

    Working on Toggle Theme Button

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const typingForm = document.querySelector('.typing-form');
 const chatlist = document.querySelector('.chat-list');
+const toggleThemeButton = document.querySelector('#toggle-theme-button');
+
+
 
 let userMessage = null;
 const YOUR_API_KEY = "AIzaSyBw1KZQppLd9O_dN9tWClBLTjyQ_DsMdZc";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${YOUR_API_KEY}`;
 
+const loadLocalstorgeData =() =>{
+  const savedchats=  localStorage.getItem("savedchats")
+  const isLightMode =(localStorage.getItem("themeColor") ==="light_mode")
+    document.body.classList.toggle("light_mode",isLightMode)
+    toggleThemeButton.innerText=isLightMode ? "dark_mode" : "light_mode"
+    chatlist.innerHTML=savedchats ||"";
+    chatlist.scrollTo(0, chatlist.scrollHeight)
+
+}
+loadLocalstorgeData();
 const createMessageElement = (content, ...classes) => {
     const div = document.createElement("div");
     div.classList.add('message', ...classes);
     div.innerHTML = content;
     return div;
 };
-const showTypingEffect=(text ,textElement)=>{
+const showTypingEffect=(text ,textElement,incomingMessageDiv)=>{
     const words = text.split(' ');
     let currentWordIndex =0;
 const typingInterval =setInterval(()=>{
 textElement.innerText+=(currentWordIndex ===0? '' : ' ')+words[currentWordIndex++]
-   if(currentWordIndex === words.length){
+incomingMessageDiv.querySelector('.icon').classList.add('hide');  
+if(currentWordIndex === words.length){
     clearInterval(typingInterval)
-   }
+    incomingMessageDiv.querySelector('.icon').classList.remove('hide');  
+    localStorage.setItem("savedchats",chatlist.innerHTML)
+}
+    chatlist.scrollTo(0, chatlist.scrollHeight)
+   
 },75);
 }
 const generateAPIResponse = async (incomingMessageDiv) => {
@@ -42,8 +60,8 @@ const generateAPIResponse = async (incomingMessageDiv) => {
         }
 
         const data = await response.json();
-        const apiResponse =data?.candidates[0].content.parts[0].text;
-         showTypingEffect(apiResponse ,textElement)
+        const apiResponse =data?.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1');
+         showTypingEffect(apiResponse ,textElement,incomingMessageDiv)
     } catch (error) {
         console.error('Error occurred:', error);
     } finally{
@@ -67,6 +85,8 @@ const showLoadingAnimation = () => {
     
     const incomingMessageDiv = createMessageElement(html, 'incoming', 'loading');
     chatlist.appendChild(incomingMessageDiv);
+
+    chatlist.scrollTo(0, chatlist.scrollHeight)
     generateAPIResponse(incomingMessageDiv);
 };
 
@@ -95,9 +115,16 @@ const handleOutgoingChat = () => {
     outgoingMessageDiv.querySelector('.text').innerHTML = userMessage;
     chatlist.appendChild(outgoingMessageDiv);
     typingForm.reset();
+    chatlist.scrollTo(0, chatlist.scrollHeight)
+
     setTimeout(showLoadingAnimation, 500);
 };
 
+toggleThemeButton.addEventListener("click",() =>{
+const isLightMode=document.body.classList.toggle("light_mode")
+localStorage.setItem("themeColor",isLightMode ? "light_mode" : "dark_mode")
+toggleThemeButton.innerText=isLightMode ? "dark_mode" : "light_mode"
+});
 typingForm.addEventListener("submit", (e) => {
     e.preventDefault();
     handleOutgoingChat();
